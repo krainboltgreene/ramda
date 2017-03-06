5e576fdf265701c9e339dcaff6bbe5cd9c07e920
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-03-06 14:41:34 -0800

    Make the invoker documentation a little more understandable to newbies like myself

--- src/invoker.js
+++ src/invoker.js
@@ -5,8 +5,9 @@ var toString = require('./toString');
 
 
 /**
- * Turns a named method with a specified arity into a function that can be
- * called directly supplied with arguments and a target object.
+ * Turns a named method (a function as an object proprty) with a specified arity 
+ * into a function that can be called directly supplied with arguments and a target 
+ * object.
  *
  * The returned function is curried and accepts `arity + 1` parameters where
  * the final parameter is the target object.
@@ -22,11 +23,28 @@ var toString = require('./toString');
  * @return {Function} A new curried function.
  * @see R.construct
  * @example
+ *      // A function with no arguments
+ *      const asJson = invoker(0, "json")
+ *      // Just like calling .then((response) => response.json())
+ *      fetch("http://example.com/index.json").then(asJson)
+ *        
  *
- *      var sliceFrom = R.invoker(1, 'slice');
+ *      // A function with one arguments
+ *      var sliceFrom = invoker(1, 'slice');
  *      sliceFrom(6, 'abcdefghijklm'); //=> 'ghijklm'
- *      var sliceFrom6 = R.invoker(2, 'slice')(6);
+ *      
+ *      // A function with two arguments
+ *      var sliceFrom6 = invoker(2, 'slice')(6);
  *      sliceFrom6(8, 'abcdefghijklm'); //=> 'gh'
+ *
+ *      // NOTE: You can't simply pass some of the arguments to the initial invoker function.
+ *      const firstCreditCardSection = invoker(2, "slice", 0, 4)
+ *      firstCreditCardSection("4242 4242 4242 4242") // => Function<...>
+ *
+ *      // Instead you must immediately invoke the invoker (better wording tbd)
+ *      const firstCreditCardSection = invoker(2, "slice")(0, 4)
+ *      firstCreditCardSection("4242 4242 4242 4242") // => "4242"
+ *
  * @symb R.invoker(0, 'method')(o) = o['method']()
  * @symb R.invoker(1, 'method')(a, o) = o['method'](a)
  * @symb R.invoker(2, 'method')(a, b, o) = o['method'](a, b)
